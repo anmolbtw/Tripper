3adccb5ba982ae0e57791800d54ca0a885d76be8
anmolbtw <ajohri_be20@thapar.edu>
2023-03-13 16:47:04 +0530

    lesgo

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css'
+import { AnalyticsWrapper } from './components/analytics';
+import Image from 'next/image'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <div className="wrapper">
+          {children}
+          <AnalyticsWrapper />
+        </div>
+        <footer className="footer">
+           <a target="_blank" rel="no-opener" href="https://github.com/dabit3/gpt-travel-advisor">
+           <Image style={{ marginTop: '2px'}} alt="code available on github" width="18" height="18" src="/github.svg" />
+           </a>
+            <p>
+            By <a target="_blank" rel="no-opener" href="https://github.com/anmolbtw">@anmolbtw</a>
+            </p>
+        </footer>
+      </body>
+    </html>
+  )
+}
